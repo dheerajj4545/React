1f83173f4dfec7681069cdded8c6a961b04c683e
Dheeraj <dheeraj@gmail.com>
2026-06-25 15:16:40 +0530

    Add project files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React | Dheeraj</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React with Vite | Dheeraj</h1>
+  )
+}
+
+export default App
