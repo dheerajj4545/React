f2d1c71d56ddabdcace3dc214a4341b71c4492bb
Dheeraj <dheeraj@gmail.com>
2026-06-26 21:47:35 +0530

    Build your own React library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Hello from "./Hello"
 function App() {
+  const username='Singh'
   return (
     <>
       <Hello/>
-      <h1>React with Vite | Dheeraj</h1>
+      <h1>React with Vite | Dheeraj {username}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Hii Dheeraj</h1>
+    </div>
+  );
+}
+
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'https//google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+
+const anotherUser='Hii Dheeraj'
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to visit google",
+  anotherUser
+);
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+createRoot(document.getElementById("root")).render(reactElement);
