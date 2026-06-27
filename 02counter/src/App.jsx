4879fdfc52bb57cab2a359c45c16681f94e98417
Dheeraj <dheeraj@gmail.com>
2026-06-27 11:36:19 +0530

    new change

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,7 +7,12 @@ import './App.css'
 function App() {
   let [counter,setCounter]=useState(0)
   const addValue=()=>{
-    if(counter<20) setCounter(counter+1)
+    if(counter<20) {
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+  }
   }
   const removeValue=()=>{
     if(counter>0) setCounter(counter-1)
