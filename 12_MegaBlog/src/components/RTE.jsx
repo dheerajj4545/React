70a0905631ad24ac3768773f97adf93f135565de
dheerajj4545 <dheerajsinghrao6@gmail.com>
2026-07-12 06:02:52 +0000

    completed

--- 12_MegaBlog/src/components/RTE.jsx
+++ 12_MegaBlog/src/components/RTE.jsx
@@ -13,39 +13,37 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
+    apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
+    initialValue={defaultValue}
+    init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "help",
+            "wordcount",
+        ],
+        toolbar:
+            "undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image media | removeformat | help",
+        content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+    }}
+    onEditorChange={onChange}
+/>
     )}
     />
 
